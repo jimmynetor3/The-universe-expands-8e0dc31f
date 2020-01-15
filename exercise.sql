1de173b127ad0da76471e2e33dd88cb33d4d8e01
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-01-15 15:04:34 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,6 @@
+use sterrenstelsel;
+INSERT INTO sterrenstelsel.planeten (naam) VALUES ('Zon')
+INSERT INTO sterrenstelsel.planeten (naam) VALUES ('Mercurius')
+INSERT INTO sterrenstelsel.planeten (naam) VALUES ('Venus')
+INSERT INTO sterrenstelsel.planeten (naam) VALUES ('Aarde')
+INSERT INTO sterrenstelsel.planeten (naam) VALUES ('Mars')
